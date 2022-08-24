bad4fa9416ec417d2317b642f157e33223fba941
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-08-24 18:07:00 +0200

    create modib_db file

---
+++ helpers/movie_db.py
@@ -0,0 +1,22 @@
+import tmdbsimple as tmdb
+
+tmdb.API_KEY = '454b6ca4172e455fe7a7d8395c10d6d9'
+
+search = tmdb.Search()
+movies = tmdb.Movies()
+
+def get_movie_data(title):
+    search.movie(query=title)
+    return search.results
+
+def get_popular_movies():
+    top_rated = movies.top_rated()['results']
+    popular = movies.popular()['results']
+
+    return [*top_rated, *popular]
+
+if __name__ == '__main__':
+    data = get_movie_data('Prey')
+    
+    popular = get_popular_movies()
+    print(popular)
