7c0faf976d884dddf6b7b1dd864731ba093d309d
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-08-24 18:52:56 +0200

    implement mongo handler

---
+++ helpers/mongo_helper.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient, ASCENDING
+
+class MongoHelper:
+    database_name = 'movie_meta'
+
+    def __init__(self):
+        self.client = MongoClient("localhost", 27018)
+        self.db = self.client[self.database_name]
+        self.collection = self.db['movies']
+
+    def insert_doc(self, data):
+        return self.collection.insert_one(data).inserted_id
+
+    def get_all_data(self):
+        return self.collection.find({}).sort("title", ASCENDING)
+
+if __name__ == '__main__':
+    test = MongoHelper()
+
+    data = test.get_all_data()
+    print([item for item in data])
